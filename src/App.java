23137b3ea271aa573a338f157260b8019e289781
Miguel Espinoza R <mespinozar001@gmail.com>
2023-05-18 02:06:13 -0400

    problemas al registrar personal. Hasta acá llegué, me dio sueño.

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import controlador.Dbcontroller;
 public class App {
     public static void main(String[] args) throws Exception {
         Dbcontroller dbc = new Dbcontroller();
-        
+        dbc.registrarPersonal("test0");
 
     }
 }
--- src/controlador/Dbcontroller.java
+++ src/controlador/Dbcontroller.java
@@ -1,6 +1,10 @@
 package controlador;
 import java.sql.*;
 
+import javax.xml.stream.events.StartElement;
+
+import modelo.Personal;
+
 public class Dbcontroller {
     //modificar argumento para pedir una ruta en el futuro
     private Connection conexion;
@@ -14,14 +18,27 @@ public class Dbcontroller {
             System.out.println("conectado y listo para usar");
         } catch (Exception e) {
             System.out.println("está cagada la conexion xD");
-            System.out.println("Dbcontroller.Dbcontroller()");
             System.out.println(e);
         }
     }
 
-    public void registrarPersonal(){
+    public void registrarPersonal(String nombre){
+        try {
+            String insert = "insert into personal(nombre_completo, hora_llegada) values(?, ?);";
+            PreparedStatement statement = conexion.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+            //statement.setNull(1, Types.INTEGER);
+            statement.setString(1, nombre);
+            statement.setNull(2, Types.DATE);
 
-        
+            int filas = statement.executeUpdate();
+            System.out.println(filas);
+            statement.close();
+            System.out.println(nombre + " registrado");
+
+        } catch (Exception e) {
+            System.out.println(e);
+            System.out.println("todo mal prri :T");
+        }
     }
 
     public void marcar(){
