1b4108c3117d9f00e0f1cb945603d5df25f79377
Miguel Espinoza R <mespinozar001@gmail.com>
2023-05-18 01:23:06 -0400

    conexion a db con ruta absoluta

--- src/controlador/Dbcontroller.java
+++ src/controlador/Dbcontroller.java
@@ -8,15 +8,24 @@ public class Dbcontroller {
     public Dbcontroller(){
         //la conexion a la db se hace al momento de crear el controlador
         try {
-            conexion = DriverManager.getConnection("jdbc:sqlite:..\\database\\asist.db");
+            Class.forName("org.sqlite.JDBC");
+            //investigar más sobre rutas relativas lpm
+            conexion = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\Miky\\Desktop\\boliche\\sw\\asistencia\\database\\asist.db");
+            System.out.println("conectado y listo para usar");
         } catch (Exception e) {
-            // TODO: handle exception
+            System.out.println("está cagada la conexion xD");
+            System.out.println("Dbcontroller.Dbcontroller()");
+            System.out.println(e);
         }
     }
 
     public void registrarPersonal(){
 
         
+    }
+
+    public void marcar(){
+
     }
 
     public Connection getConexion(){
